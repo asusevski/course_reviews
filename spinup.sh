55fa7467ebd0f389fd4e7b4afbea92be89e9f3d0
alvan <ch35li@uwaterloo.ca>
2022-09-19 22:34:56 +0000

    Fixed trainer

--- spinup.sh
+++ spinup.sh
@@ -1,2 +1,4 @@
 MY_UID="$(id -u)" MY_GID="$(id -g)" docker-compose build 
 MY_UID="$(id -u)" MY_GID="$(id -g)" docker-compose run --service-ports dsc_nlp 
+
+# jupyter notebook --ip 0.0.0.0 --allow-root --no-browser
